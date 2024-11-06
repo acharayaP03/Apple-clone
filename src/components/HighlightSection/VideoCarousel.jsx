b3a23638a6f8bce946f8bd7a90dc010d00b9d9c6
acharayaP03 <acharyaP03@gmail.com>
2024-11-07 10:46:16 +1100

    initioal slides contaier with videos

---
+++ src/components/HighlightSection/VideoCarousel.jsx
@@ -0,0 +1,49 @@
+import { useRef, useState } from "react";
+import { hightlightsSlides } from "../../constants";
+
+export default function VideoCarousel() {
+  const videoRef = useRef([]);
+  const videoSpanRef = useRef([]);
+  const videoDivRef = useRef([]);
+
+  const [video, setVideo] = useState({
+    isEnd: false,
+    startPlay: false,
+    videoId: 0,
+    isLastVideo: false,
+    isPlaying: false,
+  });
+
+  const { isEnd, startPlay, videoId, isLastVideo, isPlaying } = video;
+
+  return (
+    <>
+      <div className="flex items-center">
+        {hightlightsSlides.map((list) => (
+          <div key={list.id} id="slider" className="sm:pr-20 pr-10">
+            <div className="video-carousel_container">
+              <div className="w-full h-full flex-center rounded-3xl overflow-hidden bg-black">
+                <video
+                  autoPlay
+                  id="video"
+                  playsInline={true}
+                  muted
+                  preload="auto"
+                >
+                  <source src={list.video} type="video/mp4" />
+                </video>
+              </div>
+              <div className="absolute top-12 left-[5%] z-10">
+                {list.textLists.map((text) => (
+                  <p key={text} className="md:text-2xl text-xl font-medium">
+                    {text}
+                  </p>
+                ))}
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
