0fb318209fc5a5319086318a4151cb9d1608c641
acharayaP03 <acharyaP03@gmail.com>
2024-11-08 15:28:39 +1100

    set up for three js to interact our model

--- src/features/3DModel/Model3D.jsx
+++ src/features/3DModel/Model3D.jsx
@@ -1,8 +1,12 @@
+import { useState, useRef } from "react";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import * as THREE from "three";
+
 import Model3DView from "./Model3DView";
-import { useState } from "react";
 import { yellowImg } from "../../utils";
+import { Canvas } from "@react-three/fiber";
+import { View } from "@react-three/drei";
 
 const initialState = {
   title: "iPhone 16 pro in Natural Titanium",
@@ -14,6 +18,17 @@ export default function Model3D() {
   const [size, setSize] = useState(0);
   const [model, setModel] = useState(initialState);
 
+  // camera control for model view
+  const cameraControlSmall = useRef();
+  const cameraControllLarge = useRef();
+
+  // model
+  const small = useRef(new THREE.Group());
+  const large = useRef(new THREE.Group());
+
+  // track the current model
+  const [smallRotation, setSmallRotation] = useState(0);
+  const [largeRotation, setLargeRotation] = useState(0);
   useGSAP(() => {
     gsap.to(
       "#heading",
@@ -35,8 +50,33 @@ export default function Model3D() {
 
         <div className="flex flex-col items-center mt-5">
           <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden relative">
-            <Model3DView />
+            <Model3DView
+              index={1}
+              groupRef={small}
+              gsapType="view1"
+              controlRef={cameraControlSmall}
+              setLargeRotation={setSmallRotation}
+              item={model}
+              size={size}
+            />
+            <Model3DView
+              index={2}
+              groupRef={large}
+              gsapType="view1"
+              controlRef={cameraControllLarge}
+              setLargeRotation={setLargeRotation}
+              item={model}
+              size={size}
+            />
           </div>
+
+          <Canvas
+            className="w-full h-full"
+            style={{ position: "fixed", inset: 0, overflow: "hidden" }}
+            eventSource={document.getElementById("root")}
+          >
+            <View.Port />
+          </Canvas>
         </div>
       </div>
     </section>
