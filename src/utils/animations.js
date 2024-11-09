90423165f84bbb22b4624218586a7d67f639036a
acharayaP03 <acharyaP03@gmail.com>
2024-11-09 15:47:27 +1100

    initial ui up

---
+++ src/utils/animations.js
@@ -0,0 +1,22 @@
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+/**
+ * @description animaateWithGsap
+ * @param {HTMLElement} element
+ * @param {Object} options
+ *
+ */
+
+gsap.registerPlugin(ScrollTrigger); // its necessary to register ScrollTrigger plugin for scroll animations to
+
+export function animaateWithGsap(target, animationProps, scrollProps) {
+  return gsap.to(target, {
+    ...animationProps,
+    scrollTrigger: {
+      trigger: target,
+      top: 'top 85%',
+      toggleActions: 'restart reverse restart reverse',
+      ...scrollProps,
+    },
+  });
+}
