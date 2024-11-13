6fcd5673cbaf1f3c289a8a85b004a90b46cd4b70
acharayaP03 <acharyaP03@gmail.com>
2024-11-13 21:15:53 +1100

    this stil needs its scrollanimation fixed

--- src/components/ui/SectionHeading.jsx
+++ src/components/ui/SectionHeading.jsx
@@ -1,8 +1,23 @@
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+
 export default function SectionHeading({
   text,
   id,
   className = 'section-heading',
 }) {
+  useGSAP(() => {
+    gsap.to(
+      `#${id}`,
+      {
+        y: 0,
+        duration: 1,
+        delay: 1.5,
+        opacity: 1,
+      },
+      [],
+    );
+  });
   return (
     <h1 id={id} className={className}>
       {text}
