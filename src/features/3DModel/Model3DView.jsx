3b23fb91b85de8fed870deb4775aea9d9ee043c5
acharayaP03 <acharyaP03@gmail.com>
2024-11-13 21:20:25 +1100

    adjust all, ambientm prespective, lights and orbital control for model wiggle

--- src/features/3DModel/Model3DView.jsx
+++ src/features/3DModel/Model3DView.jsx
@@ -1,12 +1,17 @@
-import { View } from '@react-three/drei';
+import { Suspense } from 'react';
 import PropTypes from 'prop-types';
+import * as THREE from 'three';
+import { View, PerspectiveCamera, OrbitControls } from '@react-three/drei';
+
+import Lights from './Lights';
+import Iphone3DModel from './Iphone3DModel';
 
 export default function Model3DView({
   index,
   groupRef,
   gsapType,
   controlRef,
-  setLargeRotation,
+  setRotation,
   item,
   size,
 }) {
@@ -14,8 +19,39 @@ export default function Model3DView({
     <View
       index={index}
       id={gsapType}
-      className={`h-full w-full border-2 border-red-500 ${index === 2 ? 'right-[-100%]' : ''}`}
-    ></View>
+      className={`h-full w-full cursor-pointer ${index === 2 ? 'right-[-100%]' : ''}`}
+    >
+      {/* Ambient light */}
+      <ambientLight intensity={0.3} />
+      {/* Camera for 3d prespective */}
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+      {/* Lights */}
+      <Lights />
+      {/* Camera control: allows users to move mouse on phone model */}
+      <OrbitControls
+        makeDefault
+        ref={controlRef}
+        enableZoom={false}
+        enablePan={false}
+        rotateSpeed={0.5}
+        target={new THREE.Vector3(0, 0, 0)}
+        onEnd={() => setRotation(controlRef.current.azimuthalAngle())}
+      />
+      {/* Model */}
+      <group
+        ref={groupRef}
+        name={`${index === 1} ? 'small' : 'large'`}
+        position={[0, 0, 0]}
+      >
+        <Suspense fallback={<div>Loading...</div>}>
+          <Iphone3DModel
+            scale={index === 1 ? [15, 15, 15] : [17, 17, 17]}
+            item={item}
+            size={size}
+          />
+        </Suspense>
+      </group>
+    </View>
   );
 }
 
